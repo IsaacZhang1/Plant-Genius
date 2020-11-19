23902180c36f9a7e7b0985ad4ae8294984073c9b
Isaac Zhang <isaaczhang@ibm.com>
2020-11-19 10:28:15 -0800

    Initial Commit

---
+++ Plant Genius/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plant Genius
+//
+//  Created by Isaac Zhang on 11/19/20.
+//  Copyright © 2020 Isaac Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plant GeniusTests/Plant_GeniusTests.swift
@@ -0,0 +1,34 @@
+//
+//  Plant_GeniusTests.swift
+//  Plant GeniusTests
+//
+//  Created by Isaac Zhang on 11/19/20.
+//  Copyright © 2020 Isaac Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import Plant_Genius
+
+class Plant_GeniusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
